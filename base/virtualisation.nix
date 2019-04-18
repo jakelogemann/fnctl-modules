ccd82258b20b15a702e9fc1c33187f333cbc5b28
Jake Logemann <820715+jakelogemann@users.noreply.github.com>
2019-04-17 23:08:04 -0400

    Adds android/logitech udev rules.

--- base/virtualisation.nix
+++ base/virtualisation.nix
@@ -2,27 +2,34 @@
 {
 
   config = lib.mkIf config.fnctl2.enable {
-  /* Enable NixOS declarative containers. */
-  boot.enableContainers = true;
+    /* Enable NixOS declarative containers. */
+    boot.enableContainers = true;
 
-  /* Enable KVM Intel Integration. */
-  boot.kernelModules       = [ "kvm" "kvm-intel" ];
+    /* Enable KVM Intel Integration. */
+    boot.kernelModules       = [ "kvm" "kvm-intel" ];
 
-  /* Pass host's resolv conf to NixOS-containers. */
-  networking.useHostResolvConf = true;
+    /* Pass host's resolv conf to NixOS-containers. */
+    networking.useHostResolvConf = true;
 
-  virtualisation    = {
-    libvirtd.enable = false;
-    docker.enable   = true;
+    virtualisation    = {
+      libvirtd.enable = false;
+      docker.enable   = false;
+    };
+
+    # Container Tools
+    environment.systemPackages = with pkgs; [ 
+      buildah   # OCI Image builder.
+      podman    # OCI Container manager designed for k8s.
+      skaffold  # Healthier alternative to Docker Compose.
+    ];
+
+    services.dockerRegistry = {
+      enable = true;
+      enableDelete = true;
+      enableGarbageCollect = true;
+      port = 5000;
+      listenAddress = "127.0.0.1";
+    };
   };
-
-  services.dockerRegistry = {
-    enable = true;
-    enableDelete = true;
-    enableGarbageCollect = true;
-    port = 5000;
-    listenAddress = "127.0.0.1";
-  };
-};
 
 }
