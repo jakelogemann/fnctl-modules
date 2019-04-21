7252e645e11cf45c14f1004e5c60ff1f3f2b117f
Jacob Logemann <820715+jakelogemann@users.noreply.github.com>
2019-04-21 13:26:15 -0400

    Attempt to override gsettings for gnome pkgs explicitly (gross)

--- gui/gnome-packages.nix
+++ gui/gnome-packages.nix
@@ -8,41 +8,71 @@ let
 
 in
 { config = mkIf (with config.fnctl2; enable && gui.enable) {
-  environment = {
-    systemPackages = with pkgs.gnome3; [
-      gnome-backgrounds
-      polari        /* Modern IRC Client */
-      sushi         /* Lightweight file previewer */
-      dconf-editor  /* Advanced deskto config editor/viewer. */
-      totem         /* Movie player for Gnome based on GStreamer */
-      vinagre vino  /* Remote desktop viewer/server */
-      zenity        /* graphical dialog prompts */
-    ];
 
-    /* Don't install unnecessary gnome3 packages */
-    gnome3.excludePackages = (with pkgs.gnome3; [
-      accerciser
-      epiphany
-      evolution
-      evolution-data-server
-      gnome-documents
-      gnome-calendar
-      gnome-contacts
-      gnome-maps
-      gnome-music
-      gnome-online-miners
-      gnome-packagekit
-      gnome-photos
-      gnome-software
-      gnome-terminal
-      gnome-todo
-      gnome-user-docs
-      gnome-user-share
-      gnome-weather
-      tracker
-      tracker-miners
-      yelp
+  services.xserver.desktopManager.gnome3 = {
+    extraGSettingsOverridePackages = (with pkgs.gnome3; [ 
+      gnome-shell
+      gnome-shell-extensions
+      gnome-usage
+      gnome-characters
+      # gnome-common
+      gnome-session
+      gnome-control-center
+      gnome-keyring
+      gnome-logs
+      gnome-panel
+      file-roller
+      totem
+      gvfs
+      gtk
+      polari
+      vino
+      vinagre
+      dconf-editor
+      nautilus
+      gnome-nettool
+      seahorse
     ]);
   };
+
+  environment.systemPackages = (with pkgs.gnome3; [
+    gnome-backgrounds
+    polari        /* Modern IRC Client */
+    sushi         /* Lightweight file previewer */
+    dconf-editor  /* Advanced deskto config editor/viewer. */
+    totem         /* Movie player for Gnome based on GStreamer */
+    vinagre       /* Remote desktop viewer */
+    vino          /* Remote desktop server */
+    zenity        /* graphical dialog prompts */
+    gnome-themes-standard gnome-themes-extra
+    gnome-tweaks
+    gnome-tweak-tool
+  ]);
+
+  /* Don't install unnecessary gnome3 packages */
+  environment.gnome3.excludePackages = (with pkgs.gnome3; [
+    accerciser
+    epiphany
+    evolution
+    evolution-data-server
+    gnome-documents
+    gnome-calendar
+    gnome-contacts
+    gnome-maps
+    gnome-music
+    gnome-online-miners
+    gnome-packagekit
+    gnome-photos
+    gnome-software
+    gnome-terminal
+    gnome-todo
+    gnome-user-docs
+    gnome-user-share
+    gnome-weather
+    tracker
+    tracker-miners
+    yelp
+  ]);
+
 }; }
 
