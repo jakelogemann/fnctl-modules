61e2b4eb01ce0dff5f5ed47281cd24b67043fc92
christinegraham <7490259+christinegraham@users.noreply.github.com>
2019-04-25 11:38:02 -0400

    Disable broken udev rules.

--- base/systemPackages.nix
+++ base/systemPackages.nix
@@ -9,7 +9,7 @@ lib.mkIf config.fnctl2.enable {
   environment.systemPackages = with pkgs; [
     cacert
   # kexec-tools         # Tools related to the kexec Linux feature
-    android-udev-rules  # Allows android MTP devices to be connected.
-    logitech-udev-rules # Allows Logitech devices to be (properly) connected.
+    # android-udev-rules  # Allows android MTP devices to be connected.
+    # logitech-udev-rules # Allows Logitech devices to be (properly) connected.
   ];
 }
