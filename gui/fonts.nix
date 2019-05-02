233eb2f040a4b58b6610f2efda20bb9e489b17df
christinegraham <7490259+christinegraham@users.noreply.github.com>
2019-05-02 11:17:08 -0400

    Fix formatting.

--- gui/fonts.nix
+++ gui/fonts.nix
@@ -2,12 +2,13 @@
 
 { config.fonts = mkIf (with config.fnctl2; enable && gui.enable) {
   enableDefaultFonts = false;
-  enableFontDir = false;
+  enableFontDir      = false;
+
   fontconfig = {
     enable = true;
     defaultFonts.monospace = ["FuraCode Nerd Font Mono"];
     defaultFonts.sansSerif = ["FuraCode Nerd Font"];
-    defaultFonts.serif = ["FuraCode Nerd Font"];
+    defaultFonts.serif     = ["FuraCode Nerd Font"];
   };
   fonts = with pkgs; [
     nerdfonts
