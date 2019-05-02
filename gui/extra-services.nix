1bcf3dc378882427023768aa09c2d24387bc41f8
christinegraham <7490259+christinegraham@users.noreply.github.com>
2019-05-02 11:16:53 -0400

    Enable dconf.

--- gui/extra-services.nix
+++ gui/extra-services.nix
@@ -1,6 +1,5 @@
-{ config, pkgs, lib, ... }: with lib; 
+{ config, pkgs, lib, ... }: with lib;
 { config = mkIf (with config.fnctl2; enable && gui.enable) {
-
 
   /* Automatically hide the mouse cursor after being idle. */
   services.unclutter = {
@@ -8,5 +7,6 @@
     keystroke = true;
   };
 
+  programs.dconf.enable = true;
 
 }; }
