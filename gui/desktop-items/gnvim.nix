c0ef186a7e7603f45ee02e71b0288dcddef085f6
Jake Logemann <820715+jakelogemann@users.noreply.github.com>
2019-05-05 10:35:01 -0400

    temporarily remove gnvim as desktop item

--- gui/desktop-items/gnvim.nix
+++ gui/desktop-items/gnvim.nix
@@ -4,15 +4,39 @@ with (import ../gnome/_helpers.nix { inherit pkgs lib; });
 let inherit (config.fnctl2) enable gui; in {
   config = mkIf (enable && gui.enable) {
     environment.systemPackages = [(pkgs.buildEnv {
-      name = "gnvim-appitems";
+      name = "nvim-appitems";
       paths = with pkgs; [
         (makeDesktopItem {
-          name = "gnvim";
-          desktopName = "GNvim";
-          exec = openEditor { inherit (gui.defaultApps) editor; };
-          icon = ./icons/gnvim.svg;
-          type = "Application";
-          categories = "TextEditor";
+          name        = "NVIM";
+          desktopName = "NVIM";
+          comment     = "NeoVim in Alacritty";
+          exec        = "alacritty --title NVIM --class NVIM --command nvim -p5 %F";
+          icon        = ./icons/gnvim.svg;
+          type        = "Application";
+          categories  = concatStringsSep ";" [
+            "Utility"
+            "TextEditor"
+            "Development"
+            "IDE"
+          ];
+          mimeType = concatStringsSep ";" [
+            "text/plain" 
+            "inode/directory"
+            "application/x-perl"
+            "text/html"
+            "text/mathml"
+            "text/x-c++hdr"
+            "text/x-c++src"
+            "text/x-chdr"
+            "text/x-csrc"
+            "text/x-dtd"
+            "text/x-java"
+            "text/x-patch"
+            "text/x-python"
+            "text/x-sql"
+            "text/x-xsrc"
+            "text/xml"
+          ];
         })
       ];
     })];
