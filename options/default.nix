7b8eb45064400b198bfc828f81cff2443a29c079
Jake Logemann <820715+jakelogemann@users.noreply.github.com>
2019-09-02 15:05:55 -0400

    adds options for dependencies (unused, RFC)

--- options/default.nix
+++ options/default.nix
@@ -1,7 +1,51 @@
-{ config, lib, options, pkgs, ... }: with lib; {
+{ config, lib, options, pkgs, ... }: with lib;
+
+let
+  mkOptionalDependency = {
+    name, repoUrl, repoRef, enabledByDefault ? true,
+  }: {
+    enable  = mkOption {
+      description = "enable optional dependency: ${name}";
+      default = enabledByDefault;
+      type = types.bool;
+    };
+
+    repoUrl = mkOption {
+      description = "GIT repository url for ${name}";
+      default     = repoUrl;
+      type        = types.str;
+    };
+
+    repoRef = mkOption {
+      description = "";
+      default     = repoRef;
+      type        = types.str;
+    };
+  };
+
+in {
 
   options.fnctl2 = {
     enable = mkEnableOption "Enable the FnCtl modules.";
+
+    pkgs-overlay = mkOptionalDependency {
+      name    = "fnctl-pkgs-overlay";
+      repoRef = "master";
+      repoUrl = "ssh://git@gitlab.com/fnctl-nix/pkgs-overlay.git";
+    };
+
+    home-manager = mkOptionalDependency {
+      name    = "home-manager-git";
+      repoRef = "release-19.03";
+      repoUrl = "https://github.com/rycee/home-manager.git";
+    };
+
+    functions-lib = mkOptionalDependency {
+      name    = "fnctl-nix-functions-lib";
+      repoRef = "master";
+      repoUrl = "ssh://git@gitlab.com/fnctl-nix/functions.git";
+    };
+
   };
 
   imports = [
